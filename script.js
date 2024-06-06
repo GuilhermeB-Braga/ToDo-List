7cce02bce08a194664c5b0e6017a0cb1d7de7754
Guilherme Braga <bbraga.guilherme@gmail.com>
2024-06-06 20:41:42 -0300

    initial commit :crown: :penguin: :firwworks:

---
+++ script.js
@@ -0,0 +1,92 @@
+// Dom Variables
+
+const taskInput = document.querySelector("#addTaskInput")
+const addTaskButton = document.querySelector(".iconWrapper")
+
+const taskList = document.querySelector(".tasksList")
+
+// Initializating Variables
+
+let tasksArr = JSON.parse(localStorage.getItem("tasksArr")) || []
+
+// Functions
+
+function createTask (){
+
+    let taskValue = taskInput.value
+
+    if(!taskValue){
+
+
+        taskInput.focus()
+        return
+
+    }else{
+        
+        tasksArr.push(taskValue)
+        localStorage.setItem("tasksArr", JSON.stringify(tasksArr))
+        
+        location.reload()
+    }
+
+
+}
+
+function viewList() {
+
+    tasksArr.forEach((task, index)=>{
+
+        let li = document.createElement("li")
+
+        li.setAttribute("class", "taskWrapper")
+
+        li.innerHTML = `
+
+            <div class="task">
+
+                <p class="taskTitle">${task}</p>
+
+            </div>
+
+            <div class="checkBox">
+
+                <i class="hidden ph ph-check"></i>
+
+            </div>
+
+        `
+
+        taskList.appendChild(li)
+
+        const checkBox = li.querySelector(".checkBox")
+        const checkedIcon = li.querySelector(".ph-check")
+        const taskContainer = li.querySelector(".task")
+
+        checkBox.addEventListener("click", ()=>{
+            taskContainer.classList.toggle("checked")
+            checkBox.classList.toggle("checked")
+            checkedIcon.classList.toggle("hidden")
+        })
+
+        taskContainer.addEventListener("dblclick", (event)=>{
+            event.preventDefault()
+            removeTask(index)
+        })
+
+
+    })
+}
+
+function removeTask(taskIndex){
+    console.log(taskIndex);
+    tasksArr.splice(taskIndex, 1)
+    localStorage.setItem("tasksArr", JSON.stringify(tasksArr))
+    location.reload()
+    
+}
+
+// AddEventListeners
+
+addTaskButton.addEventListener("click", createTask)
+
+document.addEventListener("DOMContentLoaded", viewList)
